982127c6741bfb7e9d60d994d67b2baabec0e02c
yozhgoor <yozhgoor@outlook.com>
2026-02-23 22:18:45 +0100

    Improve Firefox settings

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -38,7 +38,6 @@
                     "browser.startup.homepage_override.mstone" = "ignore";
                     "browser.uitour.enabled" = false;
                     "trailhead.firstrun.didSeeAboutWelcome" = true;
-                    "browser.bookmarks.addedImportButton" = true;
                     "app.normandy.first_run" = false;
 
                     # Clean new tab page
@@ -46,8 +45,9 @@
                     "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
                     "browser.newtabpage.activity-stream.feeds.sections" = false;
                     "browser.newtabpage.activity-stream.feeds.shortcuts" = false;
-                    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
                     "browser.newtabpage.activity-stream.showSponsored" = false;
+                    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+                    "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = false;
                     "browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts" = false;
                     "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
                     "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = false;
@@ -57,8 +57,8 @@
                     "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
 
                     # Disable telemetry
-                    "app.shield,optoutstudies.enabled" = false;
-                    "browser.discovery,enabled" = false;
+                    "app.shield.optoutstudies.enabled" = false;
+                    "browser.discovery.enabled" = false;
                     "browser.ping-centre.telemetry" = false;
                     "datareporting.healthreport.uploadEnabled" = false;
                     "datareporting.policy.dataSubmissionEnabled" = false;
@@ -82,15 +82,12 @@
                     "privacy.trackingprotection.emailtracking.enabled" = true;
                     "privacy.trackingprotection.socialtracking.enabled" = true;
                     "privacy.globalprivacycontrol.enabled" = true;
-                    "privacy.fingerprintingProtection" = true;
+                    "privacy.resistFingerprinting" = true;
                     "dom.security.https_only_mode" = true;
                     "network.http.referer.XOriginPolicy" = 2;
                     "network.http.referer.XOriginTrimmingPolicy" = 2;
                     "network.http.referer.trimmingPolicy" = 1;
-
-                    # Disable WebRTC IP Leak
-                    # Note: This will break in-browser video calls (Meet, jitsi, etc.).
-                    "media.peerconnection.enabled" = false;
+                    "places.history.enabled" = false;
 
                     # Disable auto-play
                     "media.autoplay.default" = 5;
@@ -98,15 +95,19 @@
                     # Disable middle-click paste
                     "middlemouse.paste" = false;
 
-                    # Disable DNS prefetching
+                    # Disable link preview
+                    "browser.ml.linkPreview.enabled" = false;
+
+                    # Disable prefetching over network
                     "network.prefetch-next" = false;
                     "network.dns.disablePrefetch" = false;
+                    "network.predictor.enabled" = false;
+                    "network.predictor.enable-prefetch" = false;
 
                     # Clear on shutdown
                     "privacy.sanitizeOnShutdown" = true;
-                    "privacy.clearOnShutdown_v2.browsingHistoryAndDownloads" = true;
-                    "privacy.clearOnShutdown_v2.cookiesAndStorage" = true;
                     "privacy.clearOnShutdown_v2.formdata" = true;
+                    "privacy.clearOnShutdown_v2.browsingHistoryAndDownloads" = false;
 
                     # Disable Normandy app
                     "app.normandy.enabled" = false;
@@ -117,13 +118,10 @@
                     # Disable "save password" prompts
                     "signon.rememberSignons" = false;
                     "signon.autofillForms" = false;
-                    "signon.firefoxRelay.feature" = false;
                     "signon.generation.enabled" = false;
 
                     # Set dark theme
                     "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-                    "browser.theme.content-theme" = 0;
-                    "browser.theme.toolbar-theme" = 0;
                     "layout.css.prefers-color-scheme.content-override" = 0;
 
                     # Disable translation panel
@@ -132,20 +130,26 @@
                     # Remove unwanted suggestions and shortcuts from URL bar
                     "browser.urlbar.suggest.recentsearches" = false;
                     "browser.urlbar.suggest.topsites" = false;
+                    "browser.urlbar.suggest.engines" = false;
+                    "browser.urlbar.suggest.quickactions" = false;
                     "browser.urlbar.shortcuts.history" = false;
                     "browser.urlbar.shortcuts.actions" = false;
+
+                    # Always show bookmarks in toolbar
+                    "browser.toolbars.bookmarks.visibility" = "always";
 
                     # Hide sidebar
                     "sidebar.visibility" = "hide-sidebar";
 
                     # Fonts
-                    "font.name.monospace.x-western" = "JetBrains Mono Nerd Font";
-                    "font.name.serif.x-western" = "JetBrains Mono Nerd Font";
+                    "font.name.monospace.x-western" = "JetBrainsMono Nerd Font";
+                    "font.name.serif.x-western" = "JetBrainsMono Nerd Font";
+                    "font.name.sans-serif.x-western" = "JetBrainsMono Nerd Font";
                     "font.size.variable.x-western" = 14;
 
                     # Set default download directory
                     "browser.download.dir" = "/home/${username}/downloads";
-                    "browser.download.folderlist" = 2;
+                    "browser.download.folderList" = 2;
                     "browser.download.autoOpenPreference" = false;
                     "browser.download.alwaysOpenPanel" = false;
 
@@ -158,10 +162,17 @@
                     # Disable pocket
                     "extensions.pocket.enabled" = false;
 
+                    # Don't show preview when hovering over a tab
+                    "browser.tabs.hoverPreview.showThumbnails" = false;
+
+                    # Don't use AI to suggest tabs and name for tab groups
+                    "browser.tabs.groups.smart.userEnabled" = false;
+
+                    # Play DRM-controlled content
+                    "media.eme.enabled" = true;
+
                     # Performance
                     "browser.tabs.maxOpenBeforeWarn" = 25;
-                    "full-screen-api.transition-duration.enter" = 0;
-                    "full-screen-api.transition-duration.leave" = 0;
 
                     # Authorize use of userChrome.css
                     "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
