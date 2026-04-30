a0f92f9590e3f4f8db87c14ec850b659a383e13d
yozhgoor <yozhgoor@outlook.com>
2026-04-30 17:30:00 +0200

    Clean up `i3status` module

--- modules/xorg/i3status.nix
+++ modules/xorg/i3status.nix
@@ -15,30 +15,23 @@
       };
 
       modules = {
-        "wireless _first_" = {
-          position = 1;
-          settings = {
-            format_up = "W: %quality at %essid";
-            format_down = "W: down";
-          };
-        };
         "ethernet _first_" = {
-          position = 2;
+          position = 1;
           settings = {
             format_up = "E: %ip";
             format_down = "E: down";
           };
         };
         "cpu_usage" = {
-          position = 3;
+          position = 2;
           settings.format = "CPU %usage";
         };
         "load" = {
-          position = 4;
+          position = 3;
           settings.format = "Load %1min/%5min";
         };
         "memory" = {
-          position = 5;
+          position = 4;
           settings = {
             format = "MEM %used/%available";
             format_degraded = "MEM < %available";
@@ -46,19 +39,11 @@
           };
         };
         "disk /" = {
-          position = 6;
+          position = 5;
           settings.format = "Disk %avail";
         };
-        "battery all" = {
-          position = 7;
-          settings = {
-            format = "%status %percentage";
-            low_threshold = 30;
-            threshold_type = "percentage";
-          };
-        };
         "time" = {
-          position = 8;
+          position = 6;
           settings.format = "%d-%m-%Y %H:%M";
         };
       };
